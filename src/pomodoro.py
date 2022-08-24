503026756571fcec9c2ea5834f74fd804ff6d76f
Richi <r.rodriguez@bbva.com>
2022-08-24 08:39:38 +0200

    Set sizes and locations

--- src/pomodoro.py
+++ src/pomodoro.py
@@ -2,10 +2,12 @@
 
 from cProfile import label
 from cgitb import text
+from distutils.command.config import config
 import sys
 import os
 import sqlite3
-import tkinter as tk       # Include classic widgets
+import tkinter as tk
+from tkinter import font       # Include classic widgets
 import tkinter.ttk as ttk
 from turtle import bgcolor  # Include Themed widgets
 
@@ -15,6 +17,7 @@ console.rule("Primazon")
 
 DATABASE_SQLLITE = "pomodoro.db"
 BORDER_WIDTH = 5  # Border width of the frames
+FONT_TYPE = "Helvatical bold"
 
 
 # Set the database params for SQLAlchemy ORM library. This is due to a change in the sqlalchemy
@@ -31,14 +34,16 @@ def index(message=""):
     Log.info("Method to show index page...")
 
 
-def create_widget_time(window):
+def create_frame_time(window):
     """ Create the time panel in the left side of the screen """
     frm_time = ttk.Frame(master=window, relief=tk.RIDGE,
-                         borderwidth=BORDER_WIDTH)
-    frm_time.pack(side=tk.LEFT, fill=tk.BOTH)
+                         borderwidth=BORDER_WIDTH,
+                         style="FrameTime.TFrame")
+    frm_time.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     lbl_time = ttk.Label(master=frm_time, text="00:00")
     lbl_time.pack(side=tk.TOP)
+    lbl_time.config(font=(FONT_TYPE, 60))
 
     btn_config = ttk.Button(master=frm_time, text="Config")
     btn_config.pack(side=tk.BOTTOM)
@@ -47,11 +52,12 @@ def create_widget_time(window):
     btn_focus.pack(side=tk.BOTTOM)
 
 
-def create_widget_panel(window):
+def create_frame_panel(window):
     """ Create the result panel in the right side of the screen """
     frm_panel = ttk.Frame(master=window, relief=tk.RIDGE,
-                          borderwidth=BORDER_WIDTH)
-    frm_panel.pack(side=tk.RIGHT, fill=tk.BOTH)
+                          borderwidth=BORDER_WIDTH,
+                          style="FrameRed.TFrame")
+    frm_panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
     lbl_pomodoro = ttk.Label(master=frm_panel, text="Count Pomodoro")
     lbl_pomodoro.pack(side=tk.TOP)
@@ -74,17 +80,21 @@ def set_styles():
     # Create style used by default for all Frames
     s.configure('TFrame', background='green')
 
+    # Setting a specific style
+    s.configure("FrameTime.TFrame", background="red")
+    s.configure("FramePanel.TFrame", background="yellow")
+
 
 if __name__ == "__main__":
     Log.info("Starting pomodoro app")
 
     window_pomodoro = tk.Tk(screenName="Pomodoro")
     window_pomodoro.title("Pomodoro")
-    window_pomodoro.config(bg='red')
-    window_pomodoro.geometry('300x200')
+    window_pomodoro.geometry('350x150')
+    # window_pomodoro.config(bg='red')
 
     set_styles()
-    create_widget_time(window=window_pomodoro)
-    create_widget_panel(window=window_pomodoro)
+    create_frame_time(window=window_pomodoro)
+    create_frame_panel(window=window_pomodoro)
 
     window_pomodoro.mainloop()  # Runs the Tkinter events loop
