cf999d4b2022916449106221eff40ac39f05a5f6
Richi <r.rodriguez@bbva.com>
2022-08-30 14:44:07 +0200

    Set styles and right places

--- src/components.py
+++ src/components.py
@@ -1,9 +1,18 @@
-from cgi import test
+from distutils.command.config import config
 import tkinter as tk
+from tkinter import font
+from tkinter.font import BOLD
 import tkinter.ttk as ttk
+from turtle import bgcolor
+
+from click import style
 
 BORDER_WIDTH = 5  # Border width of the frames
-FONT_TYPE = "Helvatical bold"
+FONT_TYPE = "Arial"
+FONT_SIZE = 14
+PADDINGS = {'padx': 1, 'pady': 1}
+LOGO = './docs/static/img/logo_app.png'
+
 
 # TKinter main window
 window_pomodoro = tk.Tk(screenName="Pomodoro")
@@ -11,7 +20,7 @@ window_pomodoro = tk.Tk(screenName="Pomodoro")
 # TKinter top frame
 frm_logo = ttk.Frame(master=window_pomodoro, relief=tk.RIDGE,
                      borderwidth=BORDER_WIDTH)
-photo = tk.PhotoImage(file='./docs/static/img/logo_app.png')
+photo = tk.PhotoImage(file=LOGO)
 lbl_logo = ttk.Label(master=frm_logo, image=photo)
 
 # TKinter components left side
@@ -19,16 +28,15 @@ frm_time = ttk.Frame(master=window_pomodoro, relief=tk.RIDGE,
                      borderwidth=BORDER_WIDTH,
                      style="FrameTime.TFrame")
 lbl_time = ttk.Label(master=frm_time, text="00:00")
-btn_config = ttk.Button(master=frm_time, text="Config")
-btn_focus = ttk.Button(master=frm_time, text="Start Focus")
-btn_pause = ttk.Button(master=frm_time, text="Pause")
-btn_restart = ttk.Button(master=frm_time, text="Restart")
+btn_focus = ttk.Button(master=frm_time, text="Start Focus", style="Button.TButton")
+btn_pause = ttk.Button(master=frm_time, text="Pause", style="Button.TButton")
+btn_config = ttk.Button(master=frm_time, text="Config", style="Button.TButton")
 
 
 # TKinter components left side
 frm_panel = ttk.Frame(master=window_pomodoro, relief=tk.RIDGE,
                       borderwidth=BORDER_WIDTH,
-                      style="FrameRed.TFrame")
+                      style="FramePanel.TFrame")
 lbl_pomodoro = ttk.Label(master=frm_panel, text="Count Pomodoro")
 lbl_count_pomodoro = ttk.Label(master=frm_panel, text="0")
 
@@ -37,3 +45,73 @@ lbl_count_cycles = ttk.Label(master=frm_panel, text="0")
 
 lbl_total = ttk.Label(master=frm_panel, text="Total Cycles")
 lbl_total_cycles = ttk.Label(master=frm_panel, text="0")
+
+
+def set_frame_top():
+    """ Create the panel for logo app """
+    frm_logo.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    lbl_logo.pack()
+
+
+def set_frame_time():
+    """ Create the time panel in the left side of the screen """
+
+    frm_time.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+    lbl_time.pack(side=tk.TOP)
+    lbl_time.grid(column=0, row=0, padx=1, pady=1, columnspan=2)
+
+    btn_focus.grid(column=0, row=1, sticky=tk.NS, **PADDINGS)
+    btn_pause.grid(column=1, row=1, sticky=tk.NS, **PADDINGS)
+    btn_config.grid(column=0, row=2, sticky=tk.NS, **PADDINGS, columnspan=2)
+
+
+def set_frame_panel():
+    """ Create the result panel in the right side of the screen """
+
+    frm_panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+    lbl_pomodoro.grid(column=1, row=0, sticky=tk.NS, **PADDINGS)
+    lbl_count_pomodoro.grid(column=1, row=1, sticky=tk.NS, **PADDINGS)
+
+    lbl_cycles.grid(column=1, row=2, sticky=tk.NS, **PADDINGS)
+    lbl_count_cycles.grid(column=1, row=3, sticky=tk.NS, **PADDINGS)
+
+    lbl_total.grid(column=1, row=4, sticky=tk.NS, **PADDINGS)
+    lbl_total_cycles.grid(column=1, row=5, sticky=tk.NS, **PADDINGS)
+
+
+def set_styles():
+    style = ttk.Style()
+    # Create generic styles
+    style.configure('TFrame', background='green')
+    style.configure('TLabel', font=(FONT_TYPE, FONT_SIZE))
+    style.configure('TButton', font=(FONT_TYPE, FONT_SIZE))
+
+    # Setting a specific style
+    style.configure("FrameTime.TFrame", background="red")
+    style.configure("FramePanel.TFrame", background="yellow")
+
+    style.map('Button.TButton',
+        foreground = [('pressed','red'),('active','blue')],
+        background = [('pressed','!disabled','black'),('active','white')],
+        relief=[('pressed', 'sunken'),('!pressed', 'raised')]
+    )
+
+    # Individual styles
+    lbl_time.config(font=(FONT_TYPE, 80, BOLD))
+    lbl_count_pomodoro.config(font=(FONT_TYPE, 24, BOLD))
+    lbl_count_cycles.config(font=(FONT_TYPE, 24, BOLD))
+    lbl_total_cycles.config(font=(FONT_TYPE, 24, BOLD))
+
+
+def set_window():
+    """ Set the styles for main window """
+    window_pomodoro.title("Pomodoro")
+    # window_pomodoro.geometry('350x150')
+    window_pomodoro.config(bg='red')
+    window_pomodoro.resizable(0, 0)
+
+    # Set the column weight
+    window_pomodoro.columnconfigure(0, weight=2)
+    window_pomodoro.columnconfigure(1, weight=1)
--- src/pomodoro.py
+++ src/pomodoro.py
@@ -4,8 +4,6 @@ import os
 import time
 import sqlite3
 import tkinter as tk
-from turtle import bgcolor
-
 from click import command  # Include Themed widgets
 
 from logger import Log, console
@@ -14,6 +12,7 @@ from components import *
 console.rule("Primazon")
 
 DATABASE_SQLLITE = "pomodoro.db"
+SOUND_FILE = './docs/static/sounds/sound.mp3'
 
 
 # Set the database params for SQLAlchemy ORM library. This is due to a change in the sqlalchemy
@@ -30,70 +29,10 @@ def index(message=""):
     Log.info("Method to show index page...")
 
 
-def set_frame_top():
-    """ Create the panel for logo app """
-    frm_logo.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-    lbl_logo.pack()
-
-
-def set_frame_time():
-    """ Create the time panel in the left side of the screen """
-
-    frm_time.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-    lbl_time.pack(side=tk.TOP)
-    lbl_time.config(font=(FONT_TYPE, 60))
-    lbl_time.grid(column=0, row=0, padx=1, pady=1, columnspan=2)
-
+def set_handlers():
     btn_focus.config(command=handle_btn_focus)
-    btn_focus.grid(column=0, row=1, sticky=tk.NS, padx=1, pady=1)
-
     btn_pause.config(command=handle_btn_pause)
-    btn_pause.grid(column=1, row=1, sticky=tk.NS, padx=1, pady=1)
-
-    btn_restart.config(command=handle_btn_restart)
-    btn_restart.grid(column=0, row=2, sticky=tk.NS, padx=1, pady=1)
-
     btn_config.config(command=handle_btn_config)
-    btn_config.grid(column=1, row=2, sticky=tk.NS, padx=1, pady=1)
-
-
-def set_frame_panel():
-    """ Create the result panel in the right side of the screen """
-
-    frm_panel.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-
-    lbl_pomodoro.grid(column=1, row=0, sticky=tk.NS, padx=1, pady=1)
-    lbl_pomodoro.config(font=(FONT_TYPE, 14, 'bold'))
-    lbl_count_pomodoro.grid(column=1, row=1, sticky=tk.NS, padx=1, pady=1)
-
-    lbl_cycles.grid(column=1, row=2, sticky=tk.NS, padx=1, pady=1)
-    lbl_count_cycles.grid(column=1, row=3, sticky=tk.NS, padx=1, pady=1)
-
-    lbl_total.grid(column=1, row=4, sticky=tk.NS, padx=1, pady=1)
-    lbl_total_cycles.grid(column=1, row=5, sticky=tk.NS, padx=1, pady=1)
-
-
-def set_styles():
-    s = ttk.Style()
-    # Create style used by default for all Frames
-    s.configure('TFrame', background='green')
-
-    # Setting a specific style
-    s.configure("FrameTime.TFrame", background="red")
-    s.configure("FramePanel.TFrame", background="yellow")
-
-
-def set_window():
-    """ Set the styles for main window """
-    window_pomodoro.title("Pomodoro")
-    # window_pomodoro.geometry('350x150')
-    window_pomodoro.config(bg='red')
-    window_pomodoro.resizable(0, 0)
-
-    # Set the column weight
-    window_pomodoro.columnconfigure(0, weight=2)
-    window_pomodoro.columnconfigure(1, weight=1)
 
 
 def handle_keypress(event):
@@ -105,23 +44,22 @@ def handle_btn_focus():
     Log.info(f"Button FOCUS pressed")
 
     lbl_time.config(text="15:00")
+    window_pomodoro.update()
 
-    seconds = 900
+    seconds = 5
     while seconds > 0:
         time.sleep(1)
         seconds -= 1
-        lbl_time.config(text=f"{seconds // 60}:{seconds % 60}")
+        lbl_time.config(text=f"{seconds // 60:02d}:{seconds % 60:02d}")
         window_pomodoro.update()
 
         Log.info(f"{seconds=}")
 
+    # Play sound in Mac with native player
+    os.system("afplay " + SOUND_FILE)
 
 def handle_btn_pause():
     Log.info(f"Button PAUSE pressed")
-
-
-def handle_btn_restart():
-    Log.info(f"Button RESTART pressed")
 
 
 def handle_btn_config():
@@ -136,6 +74,7 @@ if __name__ == "__main__":
     set_frame_top()
     set_frame_time()
     set_frame_panel()
+    set_handlers()
 
     # Bind keypress event to handle_keypress()
     window_pomodoro.bind("<Key>", handle_keypress)
